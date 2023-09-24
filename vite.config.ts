b3a335983e76df615b0b3a27ecdb7f8f3e2f0916
MefAldemisov <alinnamef@gmail.com>
2023-09-24 15:15:12 +0300

    fix: vite ts errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import vue from '@vitejs/plugin-vue'
-// @ts-ignore
 import path from 'path'
 import { defineConfig } from 'vite'
 import svgLoader from 'vite-svg-loader'
@@ -8,7 +7,6 @@ export default defineConfig({
   plugins: [vue(), svgLoader({ defaultImport: 'component' })],
   resolve: {
     alias: {
-      // @ts-ignore
       '@': path.resolve(__dirname, './src'),
     },
   },
