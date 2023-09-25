8c7dfdafbda1467ad97bce72f8e4d103cd6534c5
MefAldemisov <alinnamef@gmail.com>
2023-09-25 18:44:46 +0300

    feat: delete from the table

--- src/views/ValuesTable.vue
+++ src/views/ValuesTable.vue
@@ -1,47 +1,54 @@
 <template>
-  <div>
-    <h1>Table</h1>
-    <router-link to="/value/create">
-      <AppButton
-        :btn-type="ButtonType.Success"
-        type="button"
-      >
-        Создать
-      </AppButton>
-    </router-link>
-    <AppTable
-      :columns="columns"
-      :data="data"
-    />
-  </div>
+  <AppConfirm
+    ref="confirmDeleteRef"
+    confirm-text="Удалить"
+    @confirm="deleteValue"
+  >
+    Вы уверены, что хотите удалить значение {{ store.getValue(deleteItemId) }}?
+  </AppConfirm>
+  <h1>Таблица значений</h1>
+  <router-link to="/value/create">
+    <AppButton
+      :btn-type="ButtonType.Success"
+      type="button"
+    >
+      Создать
+    </AppButton>
+  </router-link>
+  <AppTable
+    :columns="columns"
+    :data="data"
+  />
 </template>
 <script setup lang="ts">
-import { h } from 'vue'
+import { h, ref } from 'vue'
 import { RouterLink } from 'vue-router'
 
 import AppButton from '@/components/AppButton.vue'
+import AppConfirm from '@/components/AppConfirm.vue'
 import AppTable from '@/components/AppTable.vue'
-import { useValues } from '@/store/values'
+import { useOpenModal } from '@/composables/useOpenModal'
+import { useValues, Value } from '@/store/values'
 import { ButtonType, TableColumn } from '@/types'
 
-interface ValueType {
-  id: number
-  value: number
-}
+const confirmDeleteRef = ref<InstanceType<typeof AppConfirm> | undefined>()
+const confirmDeleteModalEvents = useOpenModal(confirmDeleteRef)
 
 const store = useValues()
-const data: ValueType[] = store.values
+const data: Value[] = store.values
 
-const columns: TableColumn<ValueType>[] = [
+const deleteItemId = ref<number>(-1)
+
+const columns: TableColumn<Value>[] = [
   {
     id: 0,
     title: 'Индекс',
-    render: (row: ValueType) => h('div', {}, row.id),
+    render: (row: Value) => h('div', {}, row.id),
   },
   {
     id: 1,
     title: 'Значение',
-    render: (row: ValueType) => h('div', {}, row.value),
+    render: (row: Value) => h('div', {}, row.value),
   },
   {
     id: 2,
@@ -56,7 +63,25 @@ const columns: TableColumn<ValueType>[] = [
   {
     id: 3,
     title: '',
-    render: () => h(AppButton, { btnType: ButtonType.Error }, 'Удалить'),
+    render: row =>
+      h(
+        AppButton,
+        {
+          btnType: ButtonType.Error,
+          onClick: () => {
+            deleteItemId.value = row.id
+            confirmDeleteModalEvents.open()
+          },
+        },
+        'Удалить',
+      ),
   },
 ]
+
+const deleteValue = () => store.deleteValue(deleteItemId.value)
 </script>
+<style scoped lang="css">
+table {
+  margin-top: 1rem;
+}
+</style>
